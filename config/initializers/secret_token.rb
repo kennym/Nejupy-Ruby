2a9361114b8307a0652a12270bf4dabe91466399
Kenny Meyer <knny.myer@gmail.com>
2011-07-07 16:25:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NejupyRuby::Application.config.secret_token = '9771b6081586281760c7bd0a85d09a237d13a4d70b5156eea4a290a49b4ec05de25bed1f0f777f68ff1ea362b2a80fdbe374aa78c5fbfce3f2dfabce92b17e8b'
