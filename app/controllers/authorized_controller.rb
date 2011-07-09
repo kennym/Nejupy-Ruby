1bda89a75bcf8947426b7af0bb07f323b321b3b2
Kenny Meyer <knny.myer@gmail.com>
2011-07-09 10:47:52 -0400

    Add AuthorizedController

---
+++ app/controllers/authorized_controller.rb
@@ -0,0 +1,9 @@
+class AuthorizedController < ApplicationController
+  before_filter :authenticate_user!
+  check_authorization
+  load_and_authorize_resource
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+  end
+end
