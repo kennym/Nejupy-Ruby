91b5582e1942f6f4ed15fe8c79a6123830304945
Kenny Meyer <knny.myer@gmail.com>
2011-07-14 18:14:27 -0400

    Fix logout action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ NejupyRuby::Application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/login' => 'devise/sessions#new'
-    get '/logout' => 'devise/sessions#destory'
+    get '/logout' => 'devise/sessions#destroy'
     end
 
   resources :contestant
