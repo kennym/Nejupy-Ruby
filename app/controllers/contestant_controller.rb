c7896ea464dabe16b0bd4f79e185e763f02b6fc5
Kenny Meyer <knny.myer@gmail.com>
2011-07-18 17:28:39 -0400

    Hack around for only accessing allowed controller

--- app/controllers/contestant_controller.rb
+++ app/controllers/contestant_controller.rb
@@ -2,6 +2,10 @@ class ContestantController < ApplicationController
   #load_and_authorize_resource :class => ContestantController
 
   def index
+    if !current_user.role?("contestant")
+      flash["warning"] = "You are not a contestant."
+      redirect_to :root
+    end
     @competition = current_user.competition
     @problems = @competition.problems
   end
--- app/controllers/judge_controller.rb
+++ app/controllers/judge_controller.rb
@@ -1,6 +1,12 @@
 class JudgeController < ApplicationController
 
   def index
+    // TODO: Usually CanCan should handle this
+    if !current_user.role?("judge")
+      flash["warning"] = "You are not a judge."
+      redirect_to :root
+    end
+    
     @competition = current_user.competition
     @problems = @competition.problems
     @contestants = @competition.get_contestants
