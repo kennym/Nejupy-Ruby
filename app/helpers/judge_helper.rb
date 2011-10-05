57347c65423573ddbc4bcd5f9b817b1b3dafd5d4
Kenny Meyer <knny.myer@gmail.com>
2011-10-05 09:11:08 -0300

    [JUDGE] Add relative_to template helper

--- app/helpers/judge_helper.rb
+++ app/helpers/judge_helper.rb
@@ -1,2 +1,9 @@
 module JudgeHelper
+  def relative_to(competition, time)
+    start_time = competition.start_time
+    duration = time.to_i - start_time.to_i
+    minutes = duration / 60
+    seconds = duration % 60
+    return "#{t :in} #{minutes}:#{seconds} #{t :minutes}"
+  end
 end
